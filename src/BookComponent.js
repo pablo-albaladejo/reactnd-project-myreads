b6054b1d93b693da5f409e7e4fb1b84c31163ac7
Pablo Albaladejo <pablo.albaladejo.mestre@gmail.com>
2018-02-03 21:02:18 +0100

    Fix: No cover added to BookComponent and shelf error fixed.

--- src/BookComponent.js
+++ src/BookComponent.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react"
 
 class BookComponent extends Component {
+
+    noCoverUrl = 'http://via.placeholder.com/128x193?text=No%20Cover';
+
     render() {
         const { book, changeShelf } = this.props;
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("' + book.imageLinks.smallThumbnail + '")' }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("' + (book.imageLinks ? book.imageLinks.smallThumbnail : this.noCoverUrl) + '")' }}></div>
                     <div className="book-shelf-changer">
-                        <select defaultValue={book.shelf} onChange={event => changeShelf(book, event.target.value)}>
-                            <option value="none" disabled>Move to...</option>
+                        <select defaultValue={book.shelf ? book.shelf : "none" } onChange={event => changeShelf(book, event.target.value)}>
+                            <option value="move" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
                             <option value="wantToRead">Want to Read</option>
                             <option value="read">Read</option>
